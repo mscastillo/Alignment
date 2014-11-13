9fe72134b16b88732b5b833a31201281e9b6d8c7
Manuel Sánchez Castillo <sanchez.castillo@gmail.com>
2014-11-13 13:23:50 +0000

    Update tags_mismatches.sh
    Speed improved.

--- tags_mismatches.sh
+++ tags_mismatches.sh
@@ -7,22 +7,17 @@
 
 ########################################################################
 
-
 FILES=($( ls *.needle ))
 for k in $( seq 0 $((${#FILES[@]} - 1)) ) ; do
 
 	FILE=${FILES[$k]}
-	date ; echo $FILE
-	grep -n -P "$MATCHES/(\d+)" $FILE | grep "Identity"  | cut -f1 -d: > lines.$SUFFIX.txt	
-	OUTPUT=$FILE."MATCHES_"$MATCHES.sequences ; rm -f $OUTPUT
-	while read LINE ; do
-		awk -v NUMBER=$LINE 'NR == NUMBER-6 {print ; exit}' $FILE >> $OUTPUT
-		awk -v NUMBER=$LINE 'NR == NUMBER+11 {print ; exit}' $FILE >> $OUTPUT
-		awk -v NUMBER=$LINE 'NR == NUMBER+12 {print ; exit}' $FILE >> $OUTPUT
-		awk -v NUMBER=$LINE 'NR == NUMBER+13 {print ; exit}' $FILE >> $OUTPUT
-		echo >> $OUTPUT
-	done < lines.$SUFFIX.txt
-	
+	OUTPUT=$FILE."MATCHES_"$MATCHES
+	date
+	echo $FILE
+	grep -n -P "$MATCHES/(\d+)" $FILE | grep "Identity"  | cut -f1 -d: > lines.$SUFFIX.txt
+	LINES=$( cat lines.$SUFFIX.txt | awk 'BEGIN{FS="\n";OFS="";ORS=""}{print $1-6,"p;",$1+11,",",$1+13,"p;"}' )
+	sed -n $LINES $FILE > $OUTPUT	
 	rm -f lines.$SUFFIX.txt
 	
 done
+
